a7dafa07778de9ef53da2bb865c3a7abf8f46ea8
Kiyoshiro Kawanabe <s19C1031UL@s.chibakoudai.jp>
2020-11-16 23:32:15 +0900

    Home Work of HAIRETSU

---
+++ array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  int sum=0;
+  int array[10] = {1,2,3,4,5,6,7,8,9,10};
+/*
+  for (int n=1; n<11; n++);{
+  int array[10] = {n};
+  }
+*/
+  for (int i=0; i<10; i++){
+    printf("%d\n",array[i]);
+  }
+    
+    return 0;
+  }
